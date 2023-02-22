a94044420141376e23018b116d689c165677027c
dfsabin-m <sabin@digitalflow.studio>
2023-02-22 12:55:04 +0545

    Access Token update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Instagram-Basic-Display
-PHP script to display instagram posts using API 
+PHP script to display instagram posts using API
+
+Place access token value in $access_token variable in the file instagram.php file
+'IGQVJWamlGc3R1MmdCVUE5ZAGxkRURiSjFEX3lVd0JoOXNlYTlzbVlQLUxfYzg3cHoxYXdtbWV2REhINDdBRWphR1dRcmtHVzhBUG9uMWtIVlhzYl9mX3J1Uk83cnRsMEIwWHFYc1dR'
--- instagram.php
+++ instagram.php
@@ -1,17 +1,10 @@
 <?php
-
-$request_url ="https://graph.instagram.com/17841400506687706/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=IGQVJYRVFjYm02X2Vyd2FCQlVGenRFWWI3d09WaldkS19PVVB6eXQ3bjE1NWtaZAHl5dnZA3UURrbFRqTlVlSFBEaGloa2NXTU9FNl9kMHpJZAURrcms0cE1IZAFhuaXNreUdMR0ZA1Wkpn";
+$access_token = '';
+$request_url ="https://graph.instagram.com/17841400506687706/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=". $access_token;
 $requests = file_get_contents($request_url);
-
 $insta_response = json_decode($requests);
-
-
-
-
 foreach ($insta_response->data as $media) { ?>
-
 <img src = "<?php echo $media->media_url; ?>" alt = "<?php echo $media->caption; ?>"/>
-
 <?php
 }
-    ?>
+?>
